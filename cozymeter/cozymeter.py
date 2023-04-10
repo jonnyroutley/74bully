08277b49f6aaf103273eb1e79c0c187c90e20a05
Fraser Rennie <fjrennie1@outlook.com>
2023-04-10 20:43:24 +0100

    fix service process (#48)

--- cozymeter/cozymeter.py
+++ cozymeter/cozymeter.py
@@ -6,12 +6,18 @@ import board
 import adafruit_dht
 import requests
 from LCD import LCD
+import time
 
 dhtDevice = adafruit_dht.DHT22(board.D4)
 
 url = "https://api.74bully.com/reading/add"
 # Try 10 times
 lcd = LCD()
+last_time = time.time()
+
+MINUTE = 60
+
+# very good very clever boy
 while True:
     try:
         # Print the values to the serial port
@@ -30,11 +36,17 @@ while True:
             lcd.setLine(f"Temp: {temperature:.1f}ßC", 1)
             lcd.setLine(f"Hmd:  {humidity:.1f}%", 2)
 
+            if time.time() - MINUTE >= last_time:
+                last_time = time.time()
+                r = requests.post(url, json=data)
+
+    # see below comment on line 49
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
         time.sleep(0.1)
 
+    # TODO: logging question mark? 
     except Exception as error:
         dhtDevice.exit()
         raise error
