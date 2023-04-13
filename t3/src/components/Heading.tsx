6e11e5c9580bdf615962819778d5cae976f0fdc7
Jonathan Routley <jonathan.wei.liang@gmail.com>
2023-04-13 16:01:51 +0100

    made pokemon cards

--- t3/src/components/Heading.tsx
+++ t3/src/components/Heading.tsx
@@ -8,13 +8,16 @@ const Heading = () => {
     <div className="flex flex-row items-center justify-between w-full p-6">
       <h1 className="text-4xl font-bold">74 Bully Household</h1>
       <div className="flex flex-row items-center justify-between gap-4">
-        <p className={router.pathname == "/" ? "underline" : ""}>
+        <p className={router.pathname == "/" ? "underline" : "hover:text-slate-700"}>
           <Link href="/">Home</Link>
         </p>
-        <p className={router.pathname == "/shopping-list" ? "underline" : ""}>
+        <p className={router.pathname == "/lost-property" ? "underline" : "hover:text-slate-700"}>
+          <Link href="/lost-property">Lost Property</Link>
+        </p>
+        <p className={router.pathname == "/shopping-list" ? "underline" : "hover:text-slate-700"}>
           <Link href="/shopping-list">Shopping List</Link>
         </p>
-        <p className={router.pathname == "/reviews" ? "underline" : ""}>
+        <p className={router.pathname == "/reviews" ? "underline" : "hover:text-slate-700"}>
           <Link href="/reviews">Reviews</Link>
         </p>
       </div>
--- t3/src/layouts/layout.tsx
+++ t3/src/layouts/layout.tsx
@@ -3,7 +3,7 @@ import { Heading } from "@/components/Heading"
 
 const Layout = ({ children } : { children: ReactNode}) => {
   return (
-    <div className="bg-bully min-h-screen">
+    <div className="bg-bully dark:bg-[rgb(162,0,255)] min-h-screen">
       <Heading/>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-3xl mx-auto">
