01b99fa9cea57328c3bfee95e6928a23925335f1
Jonathan Routley <jonathan.wei.liang@gmail.com>
2023-04-13 22:19:00 +0100

    setting up db

--- t3/src/components/Heading.tsx
+++ t3/src/components/Heading.tsx
@@ -8,16 +8,16 @@ const Heading = () => {
     <div className="flex flex-row items-center justify-between w-full p-6">
       <h1 className="text-4xl font-bold">74 Bully Household</h1>
       <div className="flex flex-row items-center justify-between gap-4">
-        <p className={router.pathname == "/" ? "underline" : "hover:text-slate-700"}>
+        <p className={router.pathname == "/" ? "underline" : "hover:text-[rgb(162,0,255)]"}>
           <Link href="/">Home</Link>
         </p>
-        <p className={router.pathname == "/lost-property" ? "underline" : "hover:text-slate-700"}>
+        <p className={router.pathname == "/lost-property" ? "underline" : "hover:text-[rgb(162,0,255)]"}>
           <Link href="/lost-property">Lost Property</Link>
         </p>
-        <p className={router.pathname == "/shopping-list" ? "underline" : "hover:text-slate-700"}>
+        <p className={router.pathname == "/shopping-list" ? "underline" : "hover:text-[rgb(162,0,255)]"}>
           <Link href="/shopping-list">Shopping List</Link>
         </p>
-        <p className={router.pathname == "/reviews" ? "underline" : "hover:text-slate-700"}>
+        <p className={router.pathname == "/reviews" ? "underline" : "hover:text-[rgb(162,0,255)]"}>
           <Link href="/reviews">Reviews</Link>
         </p>
       </div>
